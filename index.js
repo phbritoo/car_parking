4f8538f9d30dae336d3cbfedf89b202898c7b0b8
Paulo Brito <ext.paulo.brito@farfetch.com>
2024-04-08 18:38:15 +0100

    parking exit function

--- index.js
+++ index.js
@@ -4,24 +4,52 @@ const readline = require('readline');
 class Parking {
    constructor() {
       this.slots = [];
-  }
-
-   initializeParking(slotsCount) {
-       this.slots = Array.from({ length: slotsCount }, (_, i) => ({ number: i + 1, occupied: false, registrationNumber: null, color: null }));
-       console.log(`>> Created a parking lot with ${slotsCount} slots`);
+      this.slots = [];
+      this.registrationNumbersByColor = {};
+      this.slotNumbersByColor = {};
+      this.slotNumberByRegistrationNumber = {};
    }
 
-   carParking(registrationNumber, color) {
+   initializeParking(slotsCount) {
+      this.slots = Array.from({ length: slotsCount }, (_, i) => ({ number: i + 1, occupied: false, registrationNumber: null, color: null }));
+      console.log(`>> Created a parking lot with ${slotsCount} slots`);
+   }
+
+   parkingEntrance(registrationNumber, color) {
       const availableSlot = this.slots.find(slot => !slot.occupied);
       if (!availableSlot) {
-          console.log('>> Sorry, parking lot is full :( ');
-          return;
+         console.log('>> Sorry, parking lot is full :( ');
+         return;
       }
       availableSlot.occupied = true;
       availableSlot.registrationNumber = registrationNumber;
       availableSlot.color = color;
       console.log(`>> Allocated slot number: ${availableSlot.number}`);
-  }
+   }
+
+   parkingExit(slotNumber) {
+      const slotIndex = slotNumber - 1;
+      if (!this.slots[slotIndex] || !this.slots[slotIndex].occupied) {
+         console.log(`Slot number ${slotNumber} is already free`);
+         return;
+      }
+
+      const registrationNumber = this.slots[slotIndex].registrationNumber;
+      const color = this.slots[slotIndex].color;
+
+      delete this.slotNumberByRegistrationNumber[slotNumber];
+      this.registrationNumbersByColor[color] = this.registrationNumbersByColor[color] || [];
+      this.registrationNumbersByColor[color] = this.registrationNumbersByColor[color].filter(reg => reg !== registrationNumber);
+
+      this.slotNumbersByColor[color] = this.slotNumbersByColor[color] || [];
+      this.slotNumbersByColor[color] = this.slotNumbersByColor[color].filter(slot => slot !== slotNumber);
+
+      this.slots[slotIndex].occupied = false;
+      this.slots[slotIndex].registrationNumber = null;
+      this.slots[slotIndex].color = null;
+
+      console.log(`Slot number ${slotNumber} is free`);
+   }
 }
 
 function processCommandsFromFile(fileName) {
